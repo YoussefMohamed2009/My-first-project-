8e7b567686fdcf62707e7f6b84d7f3366a57abf8
YoussefMohamed2009 <youssefmohamed20759@gmail.com>
2026-09-20 14:52:21 +0300

    Refactor greeting and add personal information

--- My_first_code.py
+++ My_first_code.py
@@ -1,1 +1,8 @@
-print("Hello! I'm Youssef")
+name = "Youssef"
+age = 16
+goal = "KSA Scholarship 2028"
+
+print(f"Hello! I'm {name}")
+print(f"My age is {age}")
+print(f"My goal is {goal}")
+print("I started coding today!")
