f7c7ceef45416b2cd86978f88aec496c6d03bbb2
YoussefMohamed2009 <youssefmohamed20759@gmail.com>
2026-09-20 15:04:22 +0300

    Refactor goal and add future age calculations

--- My_first_code.py
+++ My_first_code.py
@@ -1,8 +1,11 @@
 name = "Youssef"
 age = 16
-goal = "KSA Scholarship 2028"
-
-print(f"Hello! I'm {name}")
-print(f"My age is {age}")
-print(f"My goal is {goal}")
-print("I started coding today!")
+goal = "KSA 2028"
+cy = 2026
+ty = 2028
+left = ty - cy
+fa = age + left
+print(f"I'm {name} {age}")
+print(f"Goal {goal}")
+print(f"Left {left}")
+print(f"Age then {fa}")
