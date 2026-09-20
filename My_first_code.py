4447ab2b7a012f1e5d655600b9177ee1150fab4f
YoussefMohamed2009 <youssefmohamed20759@gmail.com>
2026-09-20 15:18:42 +0300

    Refactor input and print statements in My_first_code.py

--- My_first_code.py
+++ My_first_code.py
@@ -1,7 +1,8 @@
 name = "Youssef"
 age = 16
-major = input("نفسك تدخل تخصص ايه في السعودية؟ ")
-
-print(f"واااو {major} تخصص جامد!")
-print(f"{name} عنده {age} سنة وهدفه {major} في KSA 2028")
-print("هتوصل ان شاء الله!")
+major = input("تخصص ايه؟ ")
+print(f"واااو {major}!")
+if "هندسة" in major:
+    print("تخصص صعب بس انت قدها!")
+else:
+    print("تخصص ممتاز!")
